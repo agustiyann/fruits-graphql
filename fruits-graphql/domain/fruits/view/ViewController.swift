1af7c2016a156ecefec6ceadb68af45c0977bfcc
agustiyan <agustiyaninfo@gmail.com>
2022-09-05 17:52:07 +0700

    connect data to UI pass

--- fruits-graphql/domain/fruits/view/ViewController.swift
+++ fruits-graphql/domain/fruits/view/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
     
@@ -15,11 +16,18 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
+    
+    private let viewModel = FruitsViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private var fruits = [FruitModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
         configureTableView()
+        bindViewModel()
+        viewModel.getFruits()
     }
 
     private func configureTableView() {
@@ -27,22 +35,63 @@ class ViewController: UIViewController {
         tableView.frame = self.view.bounds
         tableView.dataSource = self
         tableView.delegate = self
-        print("tableview")
+    }
+    
+    private func bindViewModel() {
+        viewModel
+            .fruits
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { fruitList in
+                self.fruits = fruitList
+                self.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel
+            .loading
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { loading in
+                loading ? print("loading") : print("finish")
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel
+            .error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { error in
+                if error != nil {
+                    guard let err = (error as? BaseErrorModel),
+                            let message = err.errors.first?.message
+                    else {
+                        print("error system")
+                        return
+                    }
+                    
+                    print(message)
+                }
+            })
+            .disposed(by: disposeBag)
+
     }
 
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        return fruits.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        cell.textLabel?.text = "title \(indexPath.row+1)"
+        let data = fruits[indexPath.row]
+        cell.textLabel?.text = "\(data.name)"
         return cell
     }
 }
 
-extension ViewController: UITableViewDelegate {}
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
 
