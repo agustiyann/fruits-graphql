debf9e79d3ba1b53c3515c576c5c950de52aad8b
agustiyan <agustiyaninfo@gmail.com>
2022-09-05 14:55:51 +0700

    add fruit model

---
+++ fruits-graphql/domain/model/FruitModel.swift
@@ -0,0 +1,15 @@
+//
+//  FruitModel.swift
+//  fruits-graphql
+//
+//  Created by Agus Tiyansyah Syam on 05/09/22.
+//
+
+import Foundation
+
+struct Fruit: Equatable {
+    var id: String
+    var name: String
+    var origin: String
+    var description: String
+}
