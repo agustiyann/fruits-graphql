c9135e156d5e9be00c606901ef28345ec33a4411
agustiyan <agustiyaninfo@gmail.com>
2022-09-06 13:42:15 +0700

    connect data detail fruit pass

--- fruits-graphql/domain/detail/view/FruitDetailViewController.swift
+++ fruits-graphql/domain/detail/view/FruitDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class FruitDetailViewController: UIViewController {
     
@@ -15,16 +16,90 @@ class FruitDetailViewController: UIViewController {
         return scrollView
     }()
     
-    private let titleLabel: UILabel = {
+    private let origin: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14, weight: .thin)
         return label
     }()
+    
+    private let descLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let viewModel = FruitDetailViewModel()
+    var id: String = ""
+    
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
 
-        // Do any additional setup after loading the view.
+        configureUI()
+        bindViewModel()
+        viewModel.getFruitByID(id: id)
+    }
+    
+    private func bindViewModel() {
+        viewModel
+            .fruit
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] fruit in
+                self.title = fruit?.name
+                self.origin.text = fruit?.origin
+                self.descLabel.text = fruit?.description
+            })
+            .disposed(by: disposeBag)
         
+        viewModel
+            .loading
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { loading in
+                loading ? print("loading") : print("finish")
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel
+            .error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { error in
+                if error != nil {
+                    guard let err = (error as? BaseErrorModel),
+                            let message = err.errors.first?.message
+                    else {
+                        print("error system")
+                        return
+                    }
+                    
+                    print(message)
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func configureUI() {
+        self.view.addSubview(scrollView)
+        let safeG = view.safeAreaLayoutGuide
+        
+        scrollView.leadingAnchor.constraint(equalTo: safeG.leadingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: safeG.topAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: safeG.trailingAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: safeG.bottomAnchor).isActive = true
+        
+        scrollView.addSubview(descLabel)
+        scrollView.addSubview(origin)
+        
+        origin.topAnchor.constraint(equalTo: safeG.topAnchor, constant: 10).isActive = true
+        origin.leadingAnchor.constraint(equalTo: safeG.leadingAnchor, constant: 16).isActive = true
+        origin.trailingAnchor.constraint(equalTo: safeG.trailingAnchor, constant: -16).isActive = true
+        
+        descLabel.topAnchor.constraint(equalTo: origin.bottomAnchor, constant: 4).isActive = true
+        descLabel.leadingAnchor.constraint(equalTo: safeG.leadingAnchor, constant: 16).isActive = true
+        descLabel.trailingAnchor.constraint(equalTo: safeG.trailingAnchor, constant: -16).isActive = true
     }
 
 }
--- fruits-graphql/domain/fruits/view/ViewController.swift
+++ fruits-graphql/domain/fruits/view/ViewController.swift
@@ -93,6 +93,9 @@ extension ViewController: UITableViewDataSource {
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        let detailVC = FruitDetailViewController()
+        detailVC.id = fruits[indexPath.row].id
+        self.navigationController?.pushViewController(detailVC, animated: false)
     }
 }
 
