e6aed746c8c7329383c0dd3a2ee590f51e9ef3e9
agustiyan <agustiyaninfo@gmail.com>
2022-09-07 19:33:49 +0700

    add fruit unit test pass

--- fruits-graphqlTests/domain/fruits/mock/FruitsUseCaseFactory.swift
+++ fruits-graphqlTests/domain/fruits/mock/FruitsUseCaseFactory.swift
@@ -15,12 +15,14 @@ class FruitsUseCaseFactory {
     func createInstance() -> FruitsUseCase {
         self.setupGetFruits()
         self.deleteFruitSuccess()
+        self.addFruitSuccess()
         return fruitsUseCase
     }
     
     func createInstanceFailed() -> FruitsUseCase {
         self.setupGetFruitsError()
         self.deleteFruitError()
+        self.addFruitError()
         return fruitsUseCase
     }
     
@@ -44,6 +46,44 @@ class FruitsUseCaseFactory {
     
     private func deleteFruitError() {
         given(self.fruitsUseCase.deleteFruit(id: any(), completion: any())) ~> { id, completion in
+            completion(.failure(MockError.error))
+        }
+    }
+    
+    private func addFruitSuccess() {
+        given(self.fruitsUseCase.addFruit(
+            addFruitId: any(),
+            scientificName: any(),
+            treeName: any(),
+            fruitName: any(),
+            family: any(),
+            origin: any(),
+            description: any(),
+            bloom: any(),
+            maturationFruit: any(),
+            lifeCycle: any(),
+            climaticZone: any(),
+            completion: any())
+        ) ~> { id, scName, tName, fName, family, origin, desc, bloom, maturation, lifeCycle, climatic, completion in
+            completion(.success(fruitModelMock))
+        }
+    }
+    
+    private func addFruitError() {
+        given(self.fruitsUseCase.addFruit(
+            addFruitId: any(),
+            scientificName: any(),
+            treeName: any(),
+            fruitName: any(),
+            family: any(),
+            origin: any(),
+            description: any(),
+            bloom: any(),
+            maturationFruit: any(),
+            lifeCycle: any(),
+            climaticZone: any(),
+            completion: any())
+        ) ~> { id, scName, tName, fName, family, origin, desc, bloom, maturation, lifeCycle, climatic, completion in
             completion(.failure(MockError.error))
         }
     }
---
+++ fruits-graphqlTests/domain/fruits/viewModel/AddFruitViewModelTest.swift
@@ -0,0 +1,88 @@
+//
+//  AddFruitViewModel.swift
+//  fruits-graphqlTests
+//
+//  Created by Agus Tiyansyah Syam on 07/09/22.
+//
+
+import XCTest
+import RxSwift
+@testable import fruits_graphql
+
+class AddFruitViewModelTest: XCTestCase {
+    
+    private var disposeBag: DisposeBag!
+    private var fruitsUseCaseFactory: FruitsUseCaseFactory!
+    private var addFruitViewModel: AddFruitViewModel!
+
+    override func setUp() {
+        super.setUp()
+        disposeBag = DisposeBag()
+        fruitsUseCaseFactory = FruitsUseCaseFactory()
+        addFruitViewModel = AddFruitViewModel(fruitsUseCase: fruitsUseCaseFactory.createInstance())
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        disposeBag = nil
+        fruitsUseCaseFactory = nil
+        addFruitViewModel = nil
+    }
+    
+    func testAddFruitSuccess() {
+        self.addFruitViewModel
+            .error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { error in
+                XCTAssertNil(error)
+            })
+            .disposed(by: disposeBag)
+        
+        self.addFruitViewModel
+            .fruit
+            .skip(1)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { fruit in
+                XCTAssertEqual(fruit, fruitModelMock)
+            })
+            .disposed(by: disposeBag)
+        
+        self.addFruitViewModel.addFruit(
+            addFruitId: "",
+            scientificName: "",
+            treeName: "",
+            fruitName: "",
+            family: "",
+            origin: "",
+            description: "",
+            bloom: "",
+            maturationFruit: "",
+            lifeCycle: "",
+            climaticZone: "")
+    }
+    
+    func testAddFruitFailed() {
+        self.addFruitViewModel = AddFruitViewModel(fruitsUseCase: fruitsUseCaseFactory.createInstanceFailed())
+        self.addFruitViewModel
+            .error
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { error in
+                XCTAssertNotNil(error)
+            })
+            .disposed(by: disposeBag)
+        
+        self.addFruitViewModel.addFruit(
+            addFruitId: "",
+            scientificName: "",
+            treeName: "",
+            fruitName: "",
+            family: "",
+            origin: "",
+            description: "",
+            bloom: "",
+            maturationFruit: "",
+            lifeCycle: "",
+            climaticZone: "")
+    }
+
+}
